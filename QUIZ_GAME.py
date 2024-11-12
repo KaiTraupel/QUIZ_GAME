b415e6aa74d02d8aa0bc6fef7cfcde748b2ca718
Kai Traupel <kai.traupel@student.ap.be>
2024-11-12 22:27:39 +0100

    Update QUIZ_GAME.py

--- QUIZ_GAME.py
+++ QUIZ_GAME.py
@@ -2,29 +2,39 @@ import streamlit as st
 import time
 
 # Instellingen voor de kleurrijke stijl
-st.set_page_config(page_title="Disco Dans Knikkers Quiz", page_icon="🎉", layout="centered")
+st.set_page_config(page_title="KaiHarde Quiz Game", page_icon="🎉", layout="centered")
 st.markdown(
     """
     <style>
+    /* Voeg Google Fonts toe */
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:wght@400;700&family=Raleway:wght@400;700&display=swap');
+
+    .stApp {{
+        background: url("https://i.imgur.com/OvK6DFY.png"); /* Vervang door jouw Imgur-afbeeldings-URL */
+        background-size: cover;
+        background-attachment: fixed;
+        background-position: center;
+    }}
+    
     .title {
-        font-size: 3em; 
+        font-size: 5em; 
         color: #FF6F61;
         text-align: center;
         font-weight: bold;
-        font-family: 'Comic Sans MS', cursive, sans-serif;
+        font-family: 'Raleway', sans-serif;
     }
     .subtitle {
-        font-size: 1.5em;
-        color: #4C72B0;
+        font-size: 2.5em;
+        color: #2B8EAD;
         text-align: center;
         font-weight: bold;
-        font-family: 'Comic Sans MS', cursive, sans-serif;
+        font-family: 'Raleway', sans-serif;
     }
     .question {
-        font-size: 1.2em;
+        font-size: 2em;
         color: #2B8EAD;
         font-weight: bold;
-        font-family: 'Comic Sans MS', cursive, sans-serif;
+        font-family: 'Raleway', sans-serif;
         padding: 10px;
         text-align: center;
     }
@@ -39,15 +49,17 @@ st.markdown(
 )
 # Functie voor het startmenu
 def start_menu():
-    st.markdown("<div class='title'>🎉 Welkom bij de Disco Dans Knikkers Quiz! 🎉</div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtitle'>Test je kennis en ontdek hoeveel je over mij weet!</div>", unsafe_allow_html=True)
+    st.markdown("<div class='title'>🎉 Welkom bij de KaiHarde Quiz! 🎉</div>", unsafe_allow_html=True)
+    st.markdown("<div class='subtitle'>Is zien of gij wel naar mij luistert hehe!</div>", unsafe_allow_html=True)
     st.markdown("##")
     st.markdown(
         """
-        **Spelregels:**
+        **SPELREGELS:**
         - Antwoorden zijn niet hoofdlettergevoelig.
         - Voor vragen met opties hoef je alleen de letter of het nummer te geven.
-        - Het opzoeken van antwoorden is natuurlijk verboden! 😜
+        - Het opzoeken van antwoorden is natuurlijk verboden dus daaarft ni! 😜
+        - Juist = +4
+        - Fout = -2
         """
     )
     start = st.button("🚀 Start de Quiz", key="start_button")
@@ -60,7 +72,26 @@ def quiz():
         {"vraag": "Geef de afkorting van mijn studierichting in kleine letters.", "antwoord": "iot"},
         {"vraag": "Wat is mijn lievelingsdier?", "antwoord": "pinguin"},
         {"vraag": "Welke voetbalclub is koning: Bayer Leverkusen of Bayern München?", "antwoord": "bayern münchen"},
-        # Voeg meer vragen toe zoals gewenst...
+        {"vraag": "Waar of niet waar: Ik ben tijdens een wilde zuipnacht in het ziekenhuis beland om mijn maag te laten leegpompen.", "antwoord": "niet waar"},
+        {"vraag": "Raadsel: Degene die het maakt, gebruikt het niet. Degene die het koopt, wil het niet en degene die het gebruikt, weet het niet. Wat is het?", "antwoord": "doodskist"},
+        {"vraag": "Waar of niet waar: Ik ben ooit eens naar de spoed geweest voor een splinter.", "antwoord": "waar"},
+        {"vraag": "Rangschik deze pilsies van beste naar slechtste: 1: Maes, 2: Jupiler, 3: Stella, 4: Cristal, 5: Vedett, 6: Primus (bijv. 145623)", "antwoord": "351246"},
+        {"vraag": "De Vikingen hadden een groot deel van Engeland kunnen veroveren, maar welke stad was voor hun de absolute hoofdprijs?", "antwoord": "parijs"},
+        {"vraag": "Welke kleur krijg je bij het mengen van rood en groen?", "antwoord": "geel"},
+        {"vraag": "Wat is mijn lievelingskleur?", "antwoord": "rood"},
+        {"vraag": "Welke zus staat bekend om het uitvoeren van 'De worm'?", "antwoord": "jana"},
+        {"vraag": "Waar zou ik je naartoe nemen tijdens onze huwelijksreis? A: Costa Rica, B: Hawaï, C: Cuba", "antwoord": "a"},
+        {"vraag": "Ronaldo of Messi?", "antwoord": "ronaldo"},
+        {"vraag": "Wat hoort er volgens mij niet in het rijtje: A: Ananas op pizza, B: Meloen met hesp, C: vlees met kriek.", "antwoord": "c"},
+        {"vraag": "Welke sport heb ik niet uitgeoefend: Volleybal, Turnen, Kickboks, Basketbal, Paardrijden", "antwoord": "volleybal"},
+        {"vraag": "Met welk dier zou ik ons Kirsten vergelijken: A: Konijn, B: Eekhoorn, C: Alpaca.", "antwoord": "b"},
+        {"vraag": "Vul het ontbrekende woord in bij deze songtekst: 'Now if I wrote you a love note And made you smile at every word I wrote (What would you do?) Would that make you wanna change your scene And wanna be the one on my .....?'", "antwoord": "team"},
+        {"vraag": "Het lied in de vorige vraag wordt gezongen door één van mijn favoriete artiesten. Wie is het?", "antwoord": "justin timberlake"},
+        {"vraag": "Met wat speelde ik als klein manneke het meest: A: Lego, B: Action Man, C: Playmobil.", "antwoord": "a"},
+        {"vraag": "Welke gebeurtenis heeft mij NIET doen bleiten: A: de film Titanic, B: Duitsland wint WK, C: Doos Pokémon kaarten gestolen", "antwoord": "b"},
+        {"vraag": "Doorzetting, Zelfstandigheid en positiviteit zijn dingen die ik bewonder aan jou, maar door welke ben ik echt voor jou gevallen?", "antwoord": "positiviteit"},
+        {"vraag": "Waar of niet waar: eerst melk dan pas cornflakes is my way.", "antwoord": "niet waar"},
+        {"vraag": "Welke is niet waar: 1=Ik verzamelde vroeger sigarettenpeuken, 2=Ik eet het plastic dat rond kaas hangt, 3=Tot mijn 17e dacht ik dat de radio het echt over vallende sterren had in plaats van flitspalen.", "antwoord": "1"}
     ]
     
     # Instellingen voor de score
@@ -99,13 +130,13 @@ def toon_resultaat(score, penalties, totaal_vragen):
 
     # Dynamische feedback op basis van score
     if eindscore <= 50:
-        st.markdown("<div class='subtitle'>😢 Oei, dat was niet je beste poging! Probeer het nog eens!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>😢 Motje, wa is da veur iet, ik kos men schup ier af jom! Kzen il teleurgesteld in a. Ik wil da gij is goe nadenkt over wa gij ier just in gank e gezet!</div>", unsafe_allow_html=True)
         st.balloons()
     elif 50 < eindscore <= 65:
-        st.markdown("<div class='subtitle'>🤔 Goed geprobeerd! Je was er bijna!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>🤔 Bwa wete cava wel, mor daar kan kik geen ei mee bakke ze bruur!</div>", unsafe_allow_html=True)
         st.snow()
     else:
-        st.markdown("<div class='subtitle'>❤️ Super gedaan! Je kent me goed!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>❤️ INSHALLAG! Ik kus men twiej polle me a, wete da. Kom ier dak a een bees geef!!</div>", unsafe_allow_html=True)
         st.snow()
 
     # Knop om opnieuw te spelen
